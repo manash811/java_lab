0ecd899c1af3111c2dcfb3557414c8b150beb115
MANASH <manashbanka19@gmail.com>
2025-12-09 22:38:33 +0530

    commited

---
+++ OOPJ/src/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("oopj");
+        System.out.println("Manash");
+        System.out.println("abcd");
+        System.out.println("ybygb");
+    }
+}
