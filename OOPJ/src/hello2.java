ac5e06748d38bc3d149029b08e263f0bb7172294
MANASH <manashbanka19@gmail.com>
2025-12-09 22:51:46 +0530

    Deleted hello2

--- OOPJ/src/hello2.java
+++
@@ -1,9 +0,0 @@
-public class hello2 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("oopj");
-        System.out.println("Manash");
-        System.out.println("abcd");
-        System.out.println("yb");
-    }
-}
